8ad27e4c48ecf32e199075166adb7506581948bd
Andres Colonia <andreskal@gmail.com>
2019-08-18 20:20:18 -0400

    feat: simple server with time sleep in one second

---
+++ server/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+const (
+	server = "localhost"
+	port   = "9000"
+)
+
+func main() {
+	fmt.Println(server + ":" + port)
+	listener, err := net.Listen("tcp", server+":"+port)
+
+	handleError(err)
+
+	for {
+		conn, err := listener.Accept()
+
+		handleError(err)
+
+		handleConn(conn)
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().String()+"\n")
+		handleError(err)
+		time.Sleep(1 + time.Second)
+	}
+}
+
+func handleError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
