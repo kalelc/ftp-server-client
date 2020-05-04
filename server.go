34bb725439a920b89f50b59e02cc9db0e63813a6
Andres Colonia <andreskal@gmail.com>
2020-05-03 20:37:49 -0400

    feat: make and change directory

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
 	"path/filepath"
 	"strings"
 )
@@ -35,19 +36,22 @@ func main() {
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	for {
-		command, err := bufio.NewReader(conn).ReadString('\n')
+		stream, err := bufio.NewReader(conn).ReadString('\n')
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		//time.Sleep(1 + time.Second)
+		command := strings.TrimSpace(string(stream))
+		commands := strings.Split(command, " ")
 
-		//t := time.Now()
-		//myTime := t.Format(time.RFC3339) + "\n"
-
-		if strings.TrimSpace(string(command)) == "ls" {
+		switch commands[0] {
+		case "ls":
 			conn.Write([]byte(listDirectory()))
-		} else {
+		case "mkdir":
+			conn.Write([]byte(makeDirectory(commands[1])))
+		case "cd":
+			conn.Write([]byte(changeDirectory(commands[1])))
+		default:
 			conn.Write([]byte("Command not found\n"))
 		}
 	}
@@ -60,4 +64,27 @@ func listDirectory() string {
 	}
 	result := strings.Join(files, " ")
 	return result + "\n"
+}
+
+func makeDirectory(value string) (response string) {
+	err := os.Mkdir(value, 0755)
+	if err != nil {
+		response = err.Error() + "\n"
+	} else {
+		response = value + " created\n"
+	}
+	return
+}
+
+func changeDirectory(value string) (response string) {
+	os.Chdir(value)
+
+	mydir, err := os.Getwd()
+	if err == nil {
+		response = mydir + "\n"
+	} else {
+		response = err.Error() + "\n"
+	}
+
+	return
 }
